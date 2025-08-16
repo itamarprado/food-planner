ab487d5229f673666d1c68ff638ff578feb1eeeb
Itamar <itamarpj00@hotmail.com>
2025-08-16 01:28:05 -0300

    [feat]: created PrismaIngredientRepository to implement database functionalities

---
+++ src/repositories/PrismaIngredientRepository.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "../generated/prisma/index.js";
+import type { IIngredientRepository } from "./IngredientRepository.ts";
+import { Ingredient } from "../entities/ingredient/ingredient.ts";
+
+const prisma = new PrismaClient();
+
+export class PrismaIngredientRepository implements IIngredientRepository {
+  async findByName(name: string): Promise<Ingredient | null> {
+    const ingredient = await prisma.ingredient.findUnique({ where: { name } });
+    if (!ingredient) return null;
+    return Ingredient.with(ingredient);
+  }
+
+  async findAll(): Promise<Ingredient[]> {
+    const ingredients = await prisma.ingredient.findMany();
+    return ingredients.map((i) => Ingredient.with(i));
+  }
+
+  async save(ingredient: Ingredient): Promise<void> {
+    await prisma.ingredient.create({
+      data: {
+        name: ingredient.name,
+        quantity: ingredient.quantity,
+        unit: ingredient.unit,
+      },
+    });
+  }
+}
