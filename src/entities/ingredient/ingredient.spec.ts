1deb64e3afd5f62078b50d76005bf759f8a5ace1
Itamar <itamarpj00@hotmail.com>
2025-08-16 01:22:19 -0300

    [feat]: remade Ingredient entity and its respective tests

---
+++ src/entities/ingredient/ingredient.spec.ts
@@ -0,0 +1,69 @@
+import { describe, expect, test, it } from "vitest";
+import { Ingredient } from "./ingredient.ts";
+
+describe("Ingredient Entity", () => {
+  it("should create an ingredient", () => {
+    const ingredient = Ingredient.create("Sugar", 100, "grams");
+
+    expect(ingredient).toBeInstanceOf(Ingredient);
+    expect(ingredient.name).toBe("Sugar");
+    expect(ingredient.quantity).toBe(100);
+    expect(ingredient.unit).toBe("grams");
+  });
+
+  it("should throw an error if name is empty", () => {
+    expect(() => {
+      Ingredient.create("", 100, "grams");
+    }).toThrow("Name cannot be empty");
+  });
+
+  it("should throw an error if quantity is negative", () => {
+    expect(() => {
+      Ingredient.create("Sugar", -50, "grams");
+    }).toThrow("Quantity cannot be negative");
+  });
+
+  it("should throw an error if unit is empty", () => {
+    expect(() => {
+      Ingredient.create("Sugar", 100, "");
+    }).toThrow("Unit cannot be empty");
+  });
+
+  it("should increase quantity correctly", () => {
+    const ingredient = Ingredient.create("Sugar", 100, "grams");
+    ingredient.increaseQuantity(50);
+
+    expect(ingredient.quantity).toBe(150);
+  });
+
+  it("should throw an error when increasing quantity with a negative amount", () => {
+    const ingredient = Ingredient.create("Sugar", 100, "grams");
+
+    expect(() => {
+      ingredient.increaseQuantity(-10);
+    }).toThrow("Amount to increase cannot be negative");
+  });
+
+  it("should decrease quantity correctly", () => {
+    const ingredient = Ingredient.create("Sugar", 100, "grams");
+    ingredient.decreaseQuantity(30);
+
+    expect(ingredient.quantity).toBe(70);
+  });
+
+  it("should throw an error when decreasing quantity with a negative amount", () => {
+    const ingredient = Ingredient.create("Sugar", 100, "grams");
+
+    expect(() => {
+      ingredient.decreaseQuantity(-10);
+    }).toThrow("Amount to decrease cannot be negative");
+  });
+
+  it("should throw an error when decreasing quantity below zero", () => {
+    const ingredient = Ingredient.create("Sugar", 100, "grams");
+
+    expect(() => {
+      ingredient.decreaseQuantity(150);
+    }).toThrow("Quantity cannot be decreased below zero");
+  });
+});
---
+++ src/entities/ingredient/ingredient.ts
@@ -0,0 +1,112 @@
+export type IngredientProps = {
+  name: string;
+  quantity: number;
+  unit: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export class Ingredient {
+  constructor(private props: IngredientProps) {
+    this.validate();
+  }
+
+  public static create(name: string, quantity: number, unit: string) {
+    return new Ingredient({
+      name,
+      quantity,
+      unit,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+
+  public static with(props: IngredientProps) {
+    return new Ingredient(props);
+  }
+
+  public toJSON() {
+    return {
+      name: this.name,
+      quantity: this.quantity,
+      unit: this.unit,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    };
+  }
+
+  // Basic validation logic to constructor
+  private validate() {
+    if (this.name === "") {
+      throw new Error("Name cannot be empty");
+    }
+
+    if (this.quantity < 0) {
+      throw new Error("Quantity cannot be negative");
+    }
+
+    if (this.unit === "") {
+      throw new Error("Unit cannot be empty");
+    }
+  }
+
+  public increaseQuantity(amount: number) {
+    if (amount < 0) {
+      throw new Error("Amount to increase cannot be negative");
+    }
+
+    this.props.quantity += amount;
+    this.props.updatedAt = new Date();
+  }
+
+  public decreaseQuantity(amount: number) {
+    if (amount < 0) {
+      throw new Error("Amount to decrease cannot be negative");
+    }
+
+    if (this.props.quantity - amount < 0) {
+      throw new Error("Quantity cannot be decreased below zero");
+    }
+
+    this.props.quantity -= amount;
+    this.props.updatedAt = new Date();
+  }
+
+  public updateName(newName: string) {
+    if (newName === "") {
+      throw new Error("Name cannot be empty");
+    }
+
+    this.props.name = newName;
+    this.props.updatedAt = new Date();
+  }
+
+  public updateunit(newunit: string) {
+    if (newunit === "") {
+      throw new Error("Unit cannot be empty");
+    }
+
+    this.props.unit = newunit;
+    this.props.updatedAt = new Date();
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get quantity(): number {
+    return this.props.quantity;
+  }
+
+  get unit(): string {
+    return this.props.unit;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+}
